ef067cca8fd02a77c2f91c4921a4193cb53b2da9
Bruno <47230010+BrunoDorea@users.noreply.github.com>
2022-07-25 12:05:12 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemon__image');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input__search');
+
+const btnPrev = document.querySelector('.btn-prev');
+const btnNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
+
+const fetchPokemon = async(pokemon) => {
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  
+  if(APIResponse.status === 200) {
+    const data = await APIResponse.json();
+    return data;
+  }
+}
+
+const renderPokemon = async(pokemon) => {
+
+  pokemonName.innerHTML = 'Loading...';
+  pokemonNumber.innerHTML = '';
+
+  const data = await fetchPokemon(pokemon);
+
+  if (data) {
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    searchPokemon = data.id;
+    input.value = '';
+  }
+  else {
+    pokemonName.innerHTML = 'Not found';
+    pokemonNumber.innerHTML = '';
+    pokemonImage.src = '';
+
+  }
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  renderPokemon(input.value.toLowerCase());
+});
+
+btnPrev.addEventListener('click', () => {
+  if(searchPokemon > 1) {
+  searchPokemon -= 1;
+  renderPokemon(searchPokemon);
+  }
+});
+
+btnNext.addEventListener('click', () => {
+  searchPokemon += 1;
+  renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
---
+++ readme.md
@@ -0,0 +1,39 @@
+<h1 align="center">
+  Pokédex
+</h1>
+
+<p align="center">
+  <a href="">Live Preview</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<p align="center">
+<a href="https://wakatime.com/badge/user/68660678-6b86-4b78-98df-f5f41a37e1bc/project/c401e4ea-c13e-49b1-b39b-23b4acfbe898"><img src="https://wakatime.com/badge/user/68660678-6b86-4b78-98df-f5f41a37e1bc/project/c401e4ea-c13e-49b1-b39b-23b4acfbe898.svg" alt="wakatime"></a>
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML
+- CSS
+- JS
+- [PokéAPI](https://pokeapi.co/)
+
+## 💻 Projeto
+
+Projeto para construção de um Pokédex.
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](.github/LICENSE.md) para mais detalhes.
+
+---
+
+## Aulas
+
+- Aula 1
+
+---
